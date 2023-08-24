dac39684064e67d8aaa98ae9cf6139e658ad0a0a
DhirajSakariya <DhirajSakariya@gmail.com>
2023-08-24 10:02:36 +0530

    Fixed build error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import {  useState } from 'react';
+import styled from 'styled-components';
+
+const StyledLabel = styled.div`
+text-decoration:none;
+color:#9e0054;
+`
 
 function App() {
+  const [number1,setNumber1]= useState(0);
+  const [number2,setNumber2]= useState(0);
+  const [sum,setSum]= useState();
+  function create(){
+
+    const url =`https://localhost:7002/add?num1=${number1}&num2=${number2}`;
+fetch(url, {
+      "method": "POST",
+      "headers": {
+       
+        "content-type": "application/json",
+        "accept": "application/json",
+        "Access-Control-Request-Headers": "Content-Type",
+        "Access-Control-Allow-Origin":"*",
+        "Access-Control-Allow-Methods": "POST, GET, PUT"
+      },
+      
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log("result",response);
+      setSum(response);
+    })
+    .catch(err => {
+      console.log("error",err);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <h1 className="display-4 text-center">Make An API Call</h1>
+          <form className="d-flex flex-column">
+            <label htmlFor="name">
+              Number1:
+              <input
+                name="name"
+                id="number1"
+                type="number"
+                className="form-control"
+                onChange={(e)=> setNumber1(e.target.value)}      
+                required
+                />
+            </label>
+            <label htmlFor="id">
+              Number2:
+              <input
+                name="id"
+                id="number2"
+                type="number"
+                className="form-control"
+                onChange={(e)=> setNumber2(e.target.value)}
+                />
+            </label>
+            <button className="btn btn-primary" type='button' onClick={(e) => create()}>
+              Add
+            </button>
+           <div>
+           <label>SUM:{sum}
+           </label>
+           </div>
+          </form>
+        </div>
+      </div>
     </div>
-  );
+);
 }
 
 export default App;
